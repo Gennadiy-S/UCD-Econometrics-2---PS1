627ddf4aebb04be295c787da32561a9194ff050c
Gennadiy Sudak <gennadiy.sudak@ucdconnect.ie>
2022-01-23 21:43:27 +0000

    lm summary in Excel + old code in new file format (.RScript)
    Linear models output exported to excel.
    Main code script - another file format s.t. able to run in RStudion w/o copying to a new script file

---
+++ Main Code - RScript file.R
@@ -0,0 +1,24 @@
+n <- 1000
+A <- rnorm(n)
+F <- ifelse(runif(n) > 0.5,1,0)
+D < - F
+O <- ifelse(A > median(A) & F < 0.5, 1, 0)
+Y <- -1*D + 0.66*A + O + rnorm(n)
+dataset <- data.frame(Y = Y, A = A, O = O,D = D)
+save(dataset,file = "nb2.RData")
+model1 <- lm(Y ~ D, data = dataset)
+summary(model1)
+
+save(dataset,file = "nb2.RData")
+model1 <- lm(Y ~ D, data = dataset)
+summary(model1)
+
+model2 <- lm(Y ~ D + O, data = dataset)
+summary(model2)
+
+model3 <- lm(Y ~ D + O +A, data = dataset)
+summary(model3)
+
+model4 <- lm(Y ~ D +A, data = dataset)
+summary(model4)
+
